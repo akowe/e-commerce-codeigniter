9e024dcb80e2b385711fa6a5fdf4cfa018719336
akowe <estherakowe@yahoo.com>
2022-07-05 09:18:32 +0100

    first
    complete

---
+++ application/logs/log-2021-06-08.php
@@ -0,0 +1,21 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-06-08 17:51:18 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:51:28 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:51:44 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:51:47 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:51:50 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:51:51 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:51:56 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:52:04 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:52:07 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:54:06 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:54:07 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:58:29 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:58:32 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:58:37 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 17:58:49 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 18:01:34 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 18:01:45 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 18:01:49 --> 404 Page Not Found: /index
+ERROR - 2021-06-08 18:02:15 --> 404 Page Not Found: /index
